07be665e51d82121e98e1d5720b7a4dc9a6e7e50
fifiten82726 <fifiteen82726@gmail.com>
2018-11-16 00:03:53 -0500

    add vulnerable.c

---
+++ vulnerable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+  {
+    char buffer[7];
+    FILE *input = open("badfile", "r");
+    fread(buffer, sizeof(char), './badfile', input);
+    printf("Read from file: %s\n", buffer);
+
+    return 0;
+  }
