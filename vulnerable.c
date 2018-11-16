b0fc6b82fb1c49ee234a9b42d0c857a10f0ec25b
fifiten82726 <fifiteen82726@gmail.com>
2018-11-16 07:39:38 -0500

    Update: use right bit

--- vulnerable.c
+++ vulnerable.c
@@ -4,7 +4,7 @@ int main(int argc, char *argv[])
   {
     char buffer[7];
     FILE *input = open("badfile", "r");
-    fread(buffer, sizeof(char), XX, input);
+    fread(buffer, sizeof(char), 32, input);
     printf("Read from file: %s\n", buffer);
 
     return 0;
